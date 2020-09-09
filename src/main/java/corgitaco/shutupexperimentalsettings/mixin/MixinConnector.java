e8da15f12651a73d7100b6ab47ee652dde1f8429
CorgiTaco <>
2020-09-08 22:05:32 -0700

    Init

---
+++ src/main/java/corgitaco/shutupexperimentalsettings/mixin/MixinConnector.java
@@ -0,0 +1,12 @@
+package corgitaco.shutupexperimentalsettings.mixin;
+
+import org.spongepowered.asm.mixin.Mixins;
+import org.spongepowered.asm.mixin.connect.IMixinConnector;
+
+public class MixinConnector implements IMixinConnector {
+
+    @Override
+    public void connect() {
+        Mixins.addConfiguration("shutupexperimentalsettings.mixins.json");
+    }
+}
