6b2763fa10b02e379cc4fddf88ed2b6c55c8c8aa
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-01-31 12:54:53 -0800

    Force stable life cycle always

---
+++ src/main/java/corgitaco/shutupexperimentalsettings/mixin/client/MixinServerWorldInfo.java
@@ -0,0 +1,17 @@
+package corgitaco.shutupexperimentalsettings.mixin.client;
+
+import com.mojang.serialization.Lifecycle;
+import net.minecraft.world.storage.ServerWorldInfo;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ServerWorldInfo.class)
+public class MixinServerWorldInfo {
+
+    @Inject(method = "getLifecycle", at = @At("HEAD"), cancellable = true)
+    private void forceStableLifeCycle(CallbackInfoReturnable<Lifecycle> cir) {
+        cir.setReturnValue(Lifecycle.stable());
+    }
+}
