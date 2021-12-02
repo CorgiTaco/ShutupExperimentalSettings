3e885110e7a13ad996f1bbae141d30039d4c54f3
Corgi_Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-12-01 16:27:31 -0800

    1.18

--- src/main/java/corgitaco/shutupexperimentalsettings/mixin/client/MixinServerWorldInfo.java
+++ src/main/java/corgitaco/shutupexperimentalsettings/mixin/client/MixinServerWorldInfo.java
@@ -1,16 +1,16 @@
 package corgitaco.shutupexperimentalsettings.mixin.client;
 
 import com.mojang.serialization.Lifecycle;
-import net.minecraft.world.storage.ServerWorldInfo;
+import net.minecraft.world.level.storage.PrimaryLevelData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ServerWorldInfo.class)
+@Mixin(PrimaryLevelData.class)
 public class MixinServerWorldInfo {
 
-    @Inject(method = "getLifecycle", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "worldGenSettingsLifecycle", at = @At("HEAD"), cancellable = true)
     private void forceStableLifeCycle(CallbackInfoReturnable<Lifecycle> cir) {
         cir.setReturnValue(Lifecycle.stable());
     }
