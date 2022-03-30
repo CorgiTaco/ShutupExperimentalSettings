39e5a80d8860597b1bd96823d96379b43bb3e5ee
AlcatrazEscapee <alex@molleroneill.com>
2022-03-30 13:08:31 -0400

    Annoyance fueled mod development always has the best results.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Shutup Experimental Settings!
 
 This simple mod is a forge port of the fabric mod "Disable Custom Worlds Advice" which disables the annoying "Experimental Features" advice that appears every time you create or load a world with custom dimensions or world settings. You can use it if either this screen annoys you as much as it does to me or you are creating a mod with custom worldgen!(This README matches the README found at https://github.com/rdvdev2/DisableCustomWorldsAdvice/blob/master/README.MD)
+
+Note: In 1.18.2, this mod also removes the warning symbol put there by Forge itself on any world using custom settings.
---
+++ src/main/java/corgitaco/shutupexperimentalsettings/mixin/PrimaryLevelDataMixin.java
@@ -0,0 +1,21 @@
+package corgitaco.shutupexperimentalsettings.mixin;
+
+import net.minecraft.world.level.storage.PrimaryLevelData;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PrimaryLevelData.class)
+public abstract class PrimaryLevelDataMixin
+{
+    /**
+     * Prevents Forge from showing the experimental warning screen ever, rather than showing it once per world.
+     */
+    @Inject(method = "hasConfirmedExperimentalWarning", at = @At("HEAD"), cancellable = true, remap = false)
+    private void ignoreExperimentalSettingsScreen(CallbackInfoReturnable<Boolean> cir)
+    {
+        cir.setReturnValue(true);
+    }
+}
